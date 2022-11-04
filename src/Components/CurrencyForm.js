6db5f0dc1985b2aa768797431253f60e9e03d1ad
ryanallenk <ryanallenk@gmail.com>
2022-11-03 20:48:25 -0400

    add state to currency form

--- src/Components/CurrencyForm.js
+++ src/Components/CurrencyForm.js
@@ -3,15 +3,23 @@ import React, { useState } from "react"
 export default function CurrencyForm () {
   const [amount, setAmount] = useState(0);
 
-  const update = (e) => {
-    e.preventDefault();
+  const handleUpdate = (e) => {
     setAmount(e.target.value);
-  }
+  };
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+  };
+
+  
+
   return (
     <div className="input-form" >
-      <form className="input-amount-form">
-        <input className="input-amount" type="integer" value={amount} onChange={update}></input>
+      <form className="input-amount-form" onSubmit={handleSubmit}>
+        <input className="input-amount" type="integer" value={amount} onChange={handleUpdate} ></input>
         <select className="currency-select"></select>
+        <p>Amount in USD: {amount} </p>
       </form>
     </div>
   );
